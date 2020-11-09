9f3b04bf7a68a4f68f2187d146c5c9eb77689af4
kssytsrk <kassy@dismail.de>
2020-11-09 02:41:45 +0200

    oops, wrong file, fixed

--- p39.lisp
+++ p39.lisp
@@ -1,10 +1,15 @@
 ;;;; -*-lisp-*-
 ;;;;
-;;;; p40.lisp
+;;;; p39.lisp
 
-;;;; P40 (**) Goldbach's conjecture.
-;;;; Goldbach's conjecture says that every positive even number greater than 2 is the sum of two prime numbers. Example: 28 = 5 + 23. It is one of the most famous facts in number theory that has not been proved to be correct in the general case. It has been numerically confirmed up to very large numbers (much larger than we can go with our Prolog system). Write a predicate to find the two prime numbers that sum up to a given even integer.
-;;;; Example:
-;;;; * (goldbach 28)
-;;;; (5 23)
+;;;; P39 (*) A list of prime numbers.
+;;;; Given a range of integers by its lower and upper limit, construct a list of all prime numbers in that range.
 
+(load "p31.lisp")
+
+(defun my-prime-number-list (lower-limit upper-limit)
+  "Finds all prime numbers between lower-limit and upper-limit and returns them
+   in a list."
+  (loop for i from lower-limit to upper-limit
+        if (my-is-prime i)
+          collect i))
