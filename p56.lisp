c7ac55be8972bbc88a2ab679b072dfae2cb8c4ee
kssytsrk <kassy@dismail.de>
2021-02-03 20:55:03 +0200

    solved p56

---
+++ p56.lisp
@@ -0,0 +1,18 @@
+;;;; -*-lisp-*-
+;;;;
+;;;; p56.lisp
+
+;;;; P56 (**) Symmetric binary trees
+;;;; Let us call a binary tree symmetric if you can draw a vertical line through the root node and then the right subtree is the mirror image of the left subtree. Write a predicate symmetric/1 to check whether a given binary tree is symmetric. Hint: Write a predicate mirror/2 first to check whether one tree is the mirror image of another. We are only interested in the structure, not in the contents of the nodes.
+
+(defun symmetric-p (tree)
+  (%symmetric-p (second tree) (third tree)))
+
+(defun %symmetric-p (node1 node2)
+  (cond ((or (atom node1) (atom node2))
+         (eq node1 node2))
+        (t
+         (and (%symmetric-p (second node1)
+                            (second node2))
+              (%symmetric-p (third node1)
+                            (third node2))))))
